4517c6fc104cfe067062681c05c4fc81d0ad399e
marco santimaria <marco.santimaria@edu.unito.it>
2021-11-29 12:18:44 +0100

    fixed intellij idea configuration.

---
+++ MuppetsMail/src/main/java/module-info.java
@@ -0,0 +1,21 @@
+module org.prog3.project.muppetsmail {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+
+
+    opens org.prog3.project.muppetsmail.Client to javafx.fxml;
+    opens org.prog3.project.muppetsmail.Server to javafx.fxml;
+
+    exports org.prog3.project.muppetsmail.Client;
+    exports org.prog3.project.muppetsmail.Client.Controller;
+    exports org.prog3.project.muppetsmail.Client.Model;
+
+    exports org.prog3.project.muppetsmail.Server;
+    exports org.prog3.project.muppetsmail.Server.Controller;
+    exports org.prog3.project.muppetsmail.Server.Model;
+
+    exports org.prog3.project.muppetsmail.SharedModel;
+    exports org.prog3.project.muppetsmail.SharedModel.Exceptions;
+    exports org.prog3.project.muppetsmail.Client.ViewObjs;
+}
